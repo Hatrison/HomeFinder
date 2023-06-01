ff44ebbe1056307ac6f1ce8e1dd442b21ef36a09
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-06-01 15:47:20 +0300

    Added initial files

---
+++ src/assets/index.js
@@ -0,0 +1,4 @@
+import logo from "./logo.svg";
+import homefinder from "./homefinder.svg";
+
+export { logo, homefinder };
---
+++ src/pages/login.tsx
@@ -0,0 +1,82 @@
+import { useLogin } from "@refinedev/core";
+import { useEffect, useRef } from "react";
+
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+
+import { homefinder } from "../assets";
+
+import { CredentialResponse } from "../interfaces/google";
+
+const GOOGLE_CLIENT_ID =
+  "753289711306-4qqfttbs9ujqsqtuqp4gtl8r6m7c7p38.apps.googleusercontent.com";
+
+export const Login: React.FC = () => {
+  const { mutate: login } = useLogin<CredentialResponse>();
+
+  const GoogleButton = (): JSX.Element => {
+    const divRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+      if (typeof window === "undefined" || !window.google || !divRef.current) {
+        return;
+      }
+
+      try {
+        window.google.accounts.id.initialize({
+          ux_mode: "popup",
+          client_id: GOOGLE_CLIENT_ID,
+          callback: async (res: CredentialResponse) => {
+            if (res.credential) {
+              login(res);
+            }
+          },
+        });
+        window.google.accounts.id.renderButton(divRef.current, {
+          theme: "filled_blue",
+          size: "medium",
+          type: "standard",
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }, []);
+
+    return <div ref={divRef} />;
+  };
+
+  return (
+    <Container
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Box
+        display="flex"
+        gap="36px"
+        justifyContent="center"
+        flexDirection="column"
+      >
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          <img src={homefinder} alt="HomeFinder Logo" width="250px" />
+        </div>
+
+        <GoogleButton />
+
+        <Typography align="center" color={"text.secondary"} fontSize="12px">
+          Powered by
+          <img
+            style={{ padding: "0 5px" }}
+            alt="Google"
+            src="https://refine.ams3.cdn.digitaloceanspaces.com/superplate-auth-icons%2Fgoogle.svg"
+          />
+          Google
+        </Typography>
+      </Box>
+    </Container>
+  );
+};
