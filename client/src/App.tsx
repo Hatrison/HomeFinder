04ae9f9fc7356083c1ad8366f17af2b6034e0c89
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-06-01 22:00:25 +0300

    Implemented home page and fixed routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -17,8 +17,6 @@ import {
 import CssBaseline from "@mui/material/CssBaseline";
 import GlobalStyles from "@mui/material/GlobalStyles";
 
-import { MuiInferencer } from "@pankod/refine-inferencer/mui";
-
 import dataProvider from "@refinedev/simple-rest";
 import routerProvider from "@refinedev/react-router-v6/legacy";
 import axios, { AxiosRequestConfig } from "axios";
@@ -143,28 +141,32 @@ function App() {
           resources={[
             {
               name: "property",
-              list: MuiInferencer,
+              list: AllProperties,
+              show: PropertyDetails,
+              create: CreateProperty,
+              edit: EditProperty,
               icon: <VillaOutlined />,
             },
             {
               name: "agent",
-              list: MuiInferencer,
+              list: Agents,
+              show: AgentProfile,
               icon: <PeopleAltOutlined />,
             },
             {
               name: "review",
-              list: MuiInferencer,
+              list: Home,
               icon: <StarOutlineRounded />,
             },
             {
               name: "message",
-              list: MuiInferencer,
+              list: Home,
               icon: <ChatBubbleOutline />,
             },
             {
               name: "my-profile",
               options: { label: "My Profile" },
-              list: MuiInferencer,
+              list: MyProfile,
               icon: <AccountCircleOutlined />,
             },
           ]}
