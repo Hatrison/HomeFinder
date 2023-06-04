6a31c7a39c6e843c808f928072be459804ee2ff1
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-06-04 23:30:18 +0300

    Added agents list and my profile page

--- client/src/pages/agent-profile.tsx
+++ client/src/pages/agent-profile.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const agentProfile = () => {
+const AgentProfile = () => {
   return <div>agent-profile</div>;
 };
 
-export default agentProfile;
+export default AgentProfile;
--- client/src/pages/agent.tsx
+++ client/src/pages/agent.tsx
@@ -1,7 +1,55 @@
-import React from "react";
+import { Box, Typography } from "@pankod/refine-mui";
+import axios from "axios";
 
-const agent = () => {
-  return <div>agent</div>;
+import { AgentCard } from "components";
+import { useEffect, useState } from "react";
+
+const Agents = () => {
+  const [allAgents, setAllAgents] = useState<any>([]);
+
+  const fetchUsers = async () => {
+    try {
+      const response = await axios.get("http://localhost:8080/api/v1/users");
+      setAllAgents(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#11142D">
+        Agents List
+      </Typography>
+
+      <Box
+        mt="20px"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          gap: "20px",
+          backgroundColor: "#FCFCFC",
+        }}
+      >
+        {allAgents.length > 0 &&
+          allAgents?.map((agent: any) => (
+            <AgentCard
+              key={agent._id}
+              id={agent._id}
+              name={agent.name}
+              email={agent.email}
+              avatar={agent.avatar}
+              noOfProperties={agent.allProperties.length}
+            />
+          ))}
+      </Box>
+    </Box>
+  );
 };
 
-export default agent;
+export default Agents;
