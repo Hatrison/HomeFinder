a3a027e44a86b51bf9fa7d3cebe3149e77c29689
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-06-05 13:32:46 +0300

    Added tests for AgentCard component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -5,6 +5,7 @@ import "@testing-library/jest-dom";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import { Home, AllProperties } from "./pages";
+import { AgentCard } from "./components";
 import { BrowserRouter } from "react-router-dom";
 
 jest.mock("react-apexcharts", () => ({
@@ -39,5 +40,91 @@ describe("Home component", () => {
 
     expect(totalRevenueComponent).toBeInTheDocument();
     expect(propertyReferralsComponent).toBeInTheDocument();
+  });
+});
+
+describe("AgentCard", () => {
+  const agent = {
+    id: 1,
+    name: "John Doe",
+    email: "john.doe@example.com",
+    avatar: "avatar-url",
+    noOfProperties: 5,
+  };
+
+  it("renders agent name and email", () => {
+    render(
+      <BrowserRouter>
+        <AgentCard {...agent} />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText(agent.name)).toBeInTheDocument();
+    expect(screen.getByText(agent.email)).toBeInTheDocument();
+  });
+
+  it("renders agent avatar", () => {
+    render(
+      <BrowserRouter>
+        <AgentCard {...agent} />
+      </BrowserRouter>
+    );
+
+    const avatarImage = screen.getByAltText("user");
+    expect(avatarImage).toHaveAttribute("src", agent.avatar);
+  });
+
+  it("renders agent role", () => {
+    render(
+      <BrowserRouter>
+        <AgentCard {...agent} />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText("Real-Estate Agent")).toBeInTheDocument();
+  });
+
+  it("renders agent location and contact information", () => {
+    render(
+      <BrowserRouter>
+        <AgentCard {...agent} />
+      </BrowserRouter>
+    );
+
+    expect(screen.getByText("Lviv")).toBeInTheDocument();
+    expect(screen.getByText("+380-097-545-4141")).toBeInTheDocument();
+    expect(screen.getByText("5 Properties")).toBeInTheDocument();
+  });
+
+  it("generates correct link based on the current user", () => {
+    const currentUser = { email: agent.email };
+    localStorage.setItem("user", JSON.stringify(currentUser));
+
+    render(
+      <BrowserRouter>
+        <AgentCard {...agent} />
+      </BrowserRouter>
+    );
+
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute("href", "/my-profile");
+
+    localStorage.removeItem("user");
+  });
+
+  it("generates correct link when the current user is different", () => {
+    const currentUser = { email: "another.user@example.com" };
+    localStorage.setItem("user", JSON.stringify(currentUser));
+
+    render(
+      <BrowserRouter>
+        <AgentCard {...agent} />
+      </BrowserRouter>
+    );
+
+    const link = screen.getByRole("link");
+    expect(link).toHaveAttribute("href", "/agents/show/1");
+
+    localStorage.removeItem("user");
   });
 });
