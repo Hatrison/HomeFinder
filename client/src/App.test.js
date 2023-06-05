7c6b91cbdbc136c3e4337696876278b35c681854
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-06-05 03:07:06 +0300

    Added tests for Home page

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,26 +1,43 @@
+/* eslint-disable */
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import { Home } from "./pages";
+import "@testing-library/jest-dom";
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
+import { Home, AllProperties } from "./pages";
+import { BrowserRouter } from "react-router-dom";
 
 jest.mock("react-apexcharts", () => ({
   __esModule: true,
   default: () => <div />,
 }));
 
-// test("renders learn react link", () => {
-//   // const { getByText } = render(<Home />);
-//   // const textElement = getByText(/Dashboard/i);
-//   // expect(textElement).toBeInTheDocument();
+describe("Home component", () => {
+  test("renders the dashboard title", () => {
+    const { getByText } = render(<Home />);
+    const titleElement = getByText("Dashboard");
+    expect(titleElement).toBeInTheDocument();
+  });
 
-//   const { asFragment } = render(<Home />);
-//   expect(asFragment(<Home />)).toMatchSnapshot();
-// });
+  test("renders the pie charts", () => {
+    const { getByText } = render(<Home />);
+    const propertiesForSaleChart = getByText("Properties for Sale");
+    const propertiesForRentChart = getByText("Properties for Rent");
+    const totalCustomersChart = getByText("Total customers");
+    const propertiesForCitiesChart = getByText("Properties for Cities");
 
-describe("Main Tests", () => {
-  it("Render Home component", () => {
-    render(<Home />);
-    //eslint-disable-next-line
-    screen.debug();
-    expect(screen.getByText("Dashboard")).toBeInTheDocument();
+    expect(propertiesForSaleChart).toBeInTheDocument();
+    expect(propertiesForRentChart).toBeInTheDocument();
+    expect(totalCustomersChart).toBeInTheDocument();
+    expect(propertiesForCitiesChart).toBeInTheDocument();
+  });
+
+  test("renders the total revenue and property referrals components", () => {
+    const { getByText } = render(<Home />);
+    const totalRevenueComponent = getByText("Total Revenue");
+    const propertyReferralsComponent = getByText("Property Referrals");
+
+    expect(totalRevenueComponent).toBeInTheDocument();
+    expect(propertyReferralsComponent).toBeInTheDocument();
   });
 });
